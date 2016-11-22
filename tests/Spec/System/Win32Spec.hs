064a33f49efca5d66b24b147e37dde0a6adc3e28
Ryan Scott <ryan.gl.scott@gmail.com>
2016-11-22 10:57:45 -0500

    Update Windows tests

--- tests/Spec/System/Win32Spec.hs
+++ tests/Spec/System/Win32Spec.hs
@@ -17,9 +17,11 @@ import Prelude.Compat
 import Test.Hspec (Spec, hspec, parallel)
 
 #if defined(mingw32_HOST_OS)
+import Data.Proxy (Proxy(..))
+
 import Instances.System.Win32 ()
 
-import Spec.Utils (prop_matchesTextShow)
+import Spec.Utils (matchesTextShowSpec)
 
 import System.Win32.DebugApi (DebugEventInfo, Exception)
 import System.Win32.File (BY_HANDLE_FILE_INFORMATION, WIN32_FILE_ATTRIBUTE_DATA)
@@ -27,7 +29,6 @@ import System.Win32.Info (ProcessorArchitecture, SYSTEM_INFO)
 import System.Win32.Time (FILETIME, SYSTEMTIME, TIME_ZONE_INFORMATION, TimeZoneId)
 
 import Test.Hspec (describe)
-import Test.Hspec.QuickCheck (prop)
 
 import TextShow.System.Win32 ()
 #endif
@@ -39,25 +40,25 @@ spec :: Spec
 spec = parallel $ do
 #if defined(mingw32_HOST_OS)
     describe "DebugEventInfo" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> DebugEventInfo -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy DebugEventInfo)
     describe "Exception" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> Exception -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy Exception)
     describe "BY_HANDLE_FILE_INFORMATION" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> BY_HANDLE_FILE_INFORMATION -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy BY_HANDLE_FILE_INFORMATION)
     describe "WIN32_FILE_ATTRIBUTE_DATA" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> WIN32_FILE_ATTRIBUTE_DATA -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy WIN32_FILE_ATTRIBUTE_DATA)
     describe "ProcessorArchitecture" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> ProcessorArchitecture -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy ProcessorArchitecture)
     describe "SYSTEM_INFO" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> SYSTEM_INFO -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy SYSTEM_INFO)
     describe "FILETIME" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> FILETIME -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy FILETIME)
     describe "SYSTEMTIME" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> SYSTEMTIME -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy SYSTEMTIME)
     describe "TIME_ZONE_INFORMATION" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> TIME_ZONE_INFORMATION -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy TIME_ZONE_INFORMATION)
     describe "TimeZoneId" $
-        prop "TextShow instance" (prop_matchesTextShow :: Int -> TimeZoneId -> Bool)
+        matchesTextShowSpec (Proxy :: Proxy TimeZoneId)
 #else
     pure ()
 #endif
