d1a05aae2e0ea8b599550cdcb3b768829f63b402
Ryan Scott <ryan.gl.scott@gmail.com>
2019-03-19 17:41:09 -0400

    Pare down TH tests
    Instances.Language.Haskell.TH is an absurdly complex swamp of CPP.
    But it doesn't really deserve to be this way, especially since 99%
    of the instances in that module exist only to test TH-derived
    TextShow instances, which just isn't worth the maintenance burden.
    This simplifies the module down to only test the instances that are
    implemented by hand.

--- tests/Spec/Language/Haskell/THSpec.hs
+++ tests/Spec/Language/Haskell/THSpec.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-|
 Module:      Spec.Language.Haskell.THSpec
 Copyright:   (C) 2014-2017 Ryan Scott
@@ -31,87 +30,10 @@ main = hspec spec
 
 spec :: Spec
 spec = parallel $ do
-#if MIN_VERSION_template_haskell(2,9,0)
-    describe "AnnLookup" $
-        matchesTextShowSpec (Proxy :: Proxy AnnLookup)
-    describe "AnnTarget" $
-        matchesTextShowSpec (Proxy :: Proxy AnnTarget)
-#endif
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "Bang" $
-        matchesTextShowSpec (Proxy :: Proxy Bang)
-#endif
-    describe "Body" $
-        matchesTextShowSpec (Proxy :: Proxy Body)
-    describe "Callconv" $
-        matchesTextShowSpec (Proxy :: Proxy Callconv)
-    describe "Clause" $
-        matchesTextShowSpec (Proxy :: Proxy Clause)
-    describe "Con" $
-        matchesTextShowSpec (Proxy :: Proxy Con)
-    describe "Dec" $
-        matchesTextShowSpec (Proxy :: Proxy Dec)
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "DecidedStrictness" $
-        matchesTextShowSpec (Proxy :: Proxy DecidedStrictness)
-#endif
-#if MIN_VERSION_template_haskell(2,12,0)
-    describe "DerivClause" $
-        matchesTextShowSpec (Proxy :: Proxy DerivClause)
-    describe "DerivStrategy" $
-        matchesTextShowSpec (Proxy :: Proxy DerivStrategy)
-#endif
     describe "Doc" $
         matchesTextShowSpec (Proxy :: Proxy Doc)
-    describe "Exp" $
-        matchesTextShowSpec (Proxy :: Proxy Exp)
-#if !(MIN_VERSION_template_haskell(2,13,0))
-    describe "FamFlavour" $
-        matchesTextShowSpec (Proxy :: Proxy FamFlavour)
-#endif
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "FamilyResultSig" $
-        matchesTextShowSpec (Proxy :: Proxy FamilyResultSig)
-#endif
-    describe "Fixity" $
-        matchesTextShowSpec (Proxy :: Proxy Fixity)
-    describe "FixityDirection" $
-        matchesTextShowSpec (Proxy :: Proxy FixityDirection)
-    describe "Foreign" $
-        matchesTextShowSpec (Proxy :: Proxy Foreign)
-    describe "FunDep" $
-        matchesTextShowSpec (Proxy :: Proxy FunDep)
-    describe "Guard" $
-        matchesTextShowSpec (Proxy :: Proxy Guard)
-    describe "Info" $
-        matchesTextShowSpec (Proxy :: Proxy Info)
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "InjectivityAnn" $
-        matchesTextShowSpec (Proxy :: Proxy InjectivityAnn)
-#endif
-#if MIN_VERSION_template_haskell(2,8,0)
-    describe "Inline" $
-        matchesTextShowSpec (Proxy :: Proxy Inline)
-#else
-    describe "InlineSpec" $
-        matchesTextShowSpec (Proxy :: Proxy InlineSpec)
-#endif
-    describe "Kind" $
-        matchesTextShowSpec (Proxy :: Proxy Kind)
-    describe "Lit" $
-        matchesTextShowSpec (Proxy :: Proxy Lit)
-    describe "Loc" $
-        matchesTextShowSpec (Proxy :: Proxy Loc)
-    describe "Match" $
-        matchesTextShowSpec (Proxy :: Proxy Match)
     describe "ModName" $
         matchesTextShowSpec (Proxy :: Proxy ModName)
-#if MIN_VERSION_template_haskell(2,9,0)
-    describe "Module" $
-        matchesTextShowSpec (Proxy :: Proxy Module)
-    describe "ModuleInfo" $
-        matchesTextShowSpec (Proxy :: Proxy ModuleInfo)
-#endif
     describe "Name" $
         matchesTextShowSpec (Proxy :: Proxy Name)
     describe "showbName'" $
@@ -122,68 +44,8 @@ spec = parallel $ do
         matchesTextShowSpec (Proxy :: Proxy NameSpace)
     describe "OccName" $
         matchesTextShowSpec (Proxy :: Proxy OccName)
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "Overlap" $
-        matchesTextShowSpec (Proxy :: Proxy Overlap)
-#endif
-    describe "Pat" $
-        matchesTextShowSpec (Proxy :: Proxy Pat)
-#if MIN_VERSION_template_haskell(2,12,0)
-    describe "PatSynArgs" $
-        matchesTextShowSpec (Proxy :: Proxy PatSynArgs)
-    describe "PatSynDir" $
-        matchesTextShowSpec (Proxy :: Proxy PatSynDir)
-#endif
-#if MIN_VERSION_template_haskell(2,8,0)
-    describe "Phases" $
-        matchesTextShowSpec (Proxy :: Proxy Phases)
-#endif
     describe "PkgName" $
         matchesTextShowSpec (Proxy :: Proxy PkgName)
-    describe "Pred" $
-        matchesTextShowSpec (Proxy :: Proxy Pred)
-    describe "Pragma" $
-        matchesTextShowSpec (Proxy :: Proxy Pragma)
-    describe "Range" $
-        matchesTextShowSpec (Proxy :: Proxy Range)
-#if MIN_VERSION_template_haskell(2,9,0)
-    describe "Role" $
-        matchesTextShowSpec (Proxy :: Proxy Role)
-#endif
-#if MIN_VERSION_template_haskell(2,8,0)
-    describe "RuleBndr" $
-        matchesTextShowSpec (Proxy :: Proxy RuleBndr)
-    describe "RuleMatch" $
-        matchesTextShowSpec (Proxy :: Proxy RuleMatch)
-#endif
-    describe "Safety" $
-        matchesTextShowSpec (Proxy :: Proxy Safety)
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "SourceStrictness" $
-        matchesTextShowSpec (Proxy :: Proxy SourceStrictness)
-    describe "SourceUnpackedness" $
-        matchesTextShowSpec (Proxy :: Proxy SourceUnpackedness)
-#endif
-    describe "Stmt" $
-        matchesTextShowSpec (Proxy :: Proxy Stmt)
-    describe "Strict" $
-        matchesTextShowSpec (Proxy :: Proxy Strict)
-#if MIN_VERSION_template_haskell(2,8,0)
-    describe "TyLit" $
-        matchesTextShowSpec (Proxy :: Proxy TyLit)
-#endif
-    describe "Type" $
-        matchesTextShowSpec (Proxy :: Proxy Type)
-#if MIN_VERSION_template_haskell(2,11,0)
-    describe "TypeFamilyHead" $
-        matchesTextShowSpec (Proxy :: Proxy TypeFamilyHead)
-#endif
-#if MIN_VERSION_template_haskell(2,9,0)
-    describe "TySynEqn" $
-        matchesTextShowSpec (Proxy :: Proxy TySynEqn)
-#endif
-    describe "TyVarBndr" $
-        matchesTextShowSpec (Proxy :: Proxy TyVarBndr)
 
 -- | Verifies that `showName'` and `showbName'` have the same output.
 prop_showName' :: NameIs -> Name -> Bool
