2ec526c34403522f12de66ae75e1dd035eb10a9b
Ryan Scott <ryan.gl.scott@gmail.com>
2021-10-30 08:26:47 -0400

    Make CPP for Generic instance for Lift more precise

--- tests/Instances/Control/Applicative/Trans.hs
+++ tests/Instances/Control/Applicative/Trans.hs
@@ -20,11 +20,12 @@ module Instances.Control.Applicative.Trans () where
 
 import           Control.Applicative.Backwards (Backwards(..))
 import           Control.Applicative.Lift      (Lift(..))
+import           Control.Monad.Trans.Instances ()
 
-#if __GLASGOW_HASKELL__ >= 706
+-- TODO: Remove the MIN_VERSION_transformers_compat(0,7,0) by unconditionally
+-- depending on transformers-0.7 or later
+#if !(MIN_VERSION_transformers(0,6,0)) && !(MIN_VERSION_transformers_compat(0,7,0))
 import           GHC.Generics (Generic)
-#else
-import qualified Generics.Deriving.TH as Generics (deriveAll0)
 #endif
 
 import           Instances.Utils.GenericArbitrary (genericArbitrary)
@@ -36,8 +37,8 @@ deriving instance Arbitrary (f a) => Arbitrary (Backwards f a)
 instance (Arbitrary a, Arbitrary (f a)) => Arbitrary (Lift f a) where
     arbitrary = genericArbitrary
 
-#if __GLASGOW_HASKELL__ >= 706
+-- TODO: Remove the MIN_VERSION_transformers_compat(0,7,0) by unconditionally
+-- depending on transformers-0.7 or later
+#if !(MIN_VERSION_transformers(0,6,0)) && !(MIN_VERSION_transformers_compat(0,7,0))
 deriving instance Generic (Lift f a)
-#else
-$(Generics.deriveAll0 ''Lift)
 #endif
