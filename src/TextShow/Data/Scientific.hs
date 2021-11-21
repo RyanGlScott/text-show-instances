93547dc408401f377db95edf8dda6934e5566116
Steve Mao <maochenyan@gmail.com>
2021-11-21 13:23:47 +1100

    address comments

--- src/TextShow/Data/Scientific.hs
+++ src/TextShow/Data/Scientific.hs
@@ -1,7 +1,12 @@
+{-# OPTIONS -fno-warn-orphans #-}
 module TextShow.Data.Scientific () where
-import           Data.Scientific
-import           Data.Text.Lazy.Builder.Scientific
-import           TextShow
+import Data.Scientific
+import Data.Text.Lazy.Builder.Scientific ( scientificBuilder )
+import TextShow
 
 instance TextShow Scientific where
-    showb = scientificBuilder
+        showbPrec d s
+                | coefficient s < 0 = showbParen (d > prefixMinusPrec) (scientificBuilder s)
+                | otherwise         = scientificBuilder s
+                where prefixMinusPrec :: Int
+                      prefixMinusPrec = 6
