8c2ea26758b0526fda9da8f19e6d5ac370994e32
Steve Mao <maochenyan@gmail.com>
2022-06-08 00:05:51 +1000

    fix

--- src/TextShow/Data/Aeson.hs
+++ src/TextShow/Data/Aeson.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -fno-warn-orphans #-}
+module TextShow.Data.Aeson where
 import Data.Aeson
 import qualified Data.Aeson.Key as K
 import qualified Data.Aeson.KeyMap as KM
 import Prelude ()
 import Prelude.Compat
 import TextShow
-module TextShow.Data.Aeson where
 import TextShow.Data.Scientific ()
 import TextShow.Data.Vector ()
 
