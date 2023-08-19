542691e74e87dc8a38faefbd72f886ef17c1704b
flip111 <flip101@gmail.com>
2023-08-19 21:45:34 +0200

    changes after review

--- src/TextShow/Data/VectorNonEmpty.hs
+++ src/TextShow/Data/VectorNonEmpty.hs
@@ -12,16 +12,16 @@ Portability: GHC
 -}
 module TextShow.Data.VectorNonEmpty () where
 
-import qualified Data.Vector.NonEmpty as B (NonEmptyVector, toList)
+import qualified Data.Vector.NonEmpty as B (NonEmptyVector, toVector)
+
+import           TextShow.Data.Vector ()
 
 import           TextShow (TextShow(..), TextShow1(..), showbPrec1)
-import           TextShow.Utils (showbUnaryListWith)
 
 instance TextShow a => TextShow (B.NonEmptyVector a) where
     showbPrec = showbPrec1
     {-# INLINE showbPrec #-}
 
 instance TextShow1 B.NonEmptyVector where
-    liftShowbPrec _ sl p = showbUnaryListWith sl p . B.toList
+    liftShowbPrec sp sl p = liftShowbPrec sp sl p . B.toVector
     {-# INLINE liftShowbPrec #-}
-
